6322a9a58e4ec1928eb69d606ef2bf318bb61a2f
Adrian Murage <adrianmurage21@gmail.com>
2023-07-05 14:45:59 +0300

    setup auth to github with jwt

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,95 @@
-import Image from 'next/image'
+import jwt from 'jsonwebtoken';
 
-export default function Home() {
+export default async function Home() {
+  let accessToken;
+  function getGitHubJWT() {
+    if (!process.env.GITHUB_APP_ID || !process.env.GITHUB_APP_PK_PEM) {
+      throw new Error(
+        'GITHUB_APP_ID and GITHUB_APP_PK_PEM must be defined in .env.local'
+      );
+    }
+    return jwt.sign(
+      {
+        iat: Math.floor(Date.now() / 1000) - 60,
+        iss: process.env.GITHUB_APP_ID,
+        exp: Math.floor(Date.now() / 1000) + 60 * 10, // 10 minutes is the max
+      },
+      process.env.GITHUB_APP_PK_PEM.replace(/\\n/g, '\n'),
+      {
+        algorithm: 'RS256',
+      }
+    );
+  }
+
+  function createGitHubRequest(path, token, opts) {
+    return fetch(`https://api.github.com${path}`, {
+      ...opts,
+      headers: {
+        // ...opts.headers,
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json/github.v3.json',
+      },
+    });
+  }
+
+  async function fetchGitHub(path, token, opts) {
+    let req = await createGitHubRequest(path, token, opts);
+
+    if (req.status === 401) {
+      // JWT has expired, cache a new token
+      accessToken = await setAccessToken();
+      // Retry request with new cached access token
+      req = await createGitHubRequest(path, accessToken, opts);
+    }
+
+    return req.json();
+  }
+
+  async function getInstallation(token) {
+    const installations = await fetchGitHub('/app/installations', token);
+    const installation = Array.from(installations).find(
+      (installation) => installation.account.login === 'adrianmurage'
+    );
+    console.log({ installation });
+
+    return installation;
+  }
+
+  async function getAccessToken(installationId, token) {
+    const data = await fetchGitHub(
+      `/app/installations/${installationId}/access_tokens`,
+      token,
+      { method: 'POST' }
+    );
+    return data.token;
+  }
+
+  async function setAccessToken() {
+    const jwt = getGitHubJWT();
+    const installation = await getInstallation(jwt);
+    accessToken = await getAccessToken(installation.id, jwt);
+
+    return accessToken;
+  }
+
+  async function fetchIssues() {
+    const issues = await fetchGitHub(
+      '/repos/adrianmurage/ISR-Next-13/issues',
+      accessToken
+    );
+    return issues;
+  }
+
+  const issues = await fetchIssues();
+  console.log({ issues });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div>
+        <div>This app demonstrates how ISR works in Next.js 13.4.8</div>
+        <p>There are {issues.length} issues</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
